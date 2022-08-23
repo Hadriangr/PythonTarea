2add6f325053ef2ff0bc0304df318bd0e5eeaa9d
Hadriangr <101576079+Hadriangr@users.noreply.github.com>
2022-08-23 18:27:58 -0400

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 18:24:43 2022
+
+@author: hadri
+"""
+
+print("Hola soy Hadrian")
+print("Estoy empezando el curso de python")
+print("Espero aprender mucho")
