5ffc29a90144788aed8ed17cdc2ad9ab4c6d2c2b
Hadriangr <101576079+Hadriangr@users.noreply.github.com>
2022-08-25 15:08:15 -0400

    Add files via upload

---
+++ Python5.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 15:00:40 2022
+
+@author: hadri
+"""
+
+Nombre= 'Hadrian'
+Apellidos = 'Guevara Rodriguez'
+Edad= 29
+Email= "hadrian@gmail.com"
+Telefono= 940243192
+Casado= False
+Hijos= False
+Amigos= {'Vicente','Francisco', 'Jesus','Ulises','Leonardo'}
+Peliculas={'Pelicula1': 'Eternal Sunshine of the Spotless Mind', 'Pelicula2': 'Scott Pilgrim Vs the World'}
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(Hijos)
+print(Amigos)
+print(Peliculas)
